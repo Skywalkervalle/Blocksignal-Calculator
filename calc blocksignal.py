b6c76c585f7733753dd84c058fe9ca3344b7c492
Skywalkervalle <64580908+Skywalkervalle@users.noreply.github.com>
2025-03-19 09:17:45 +0100

    Changes and some new functions

--- calc blocksignal.py
+++ calc blocksignal.py
@@ -1,4 +1,4 @@
-def calc_blockdistance_and_time(speed_kmh, brakedelay, safetydistance):
+def calc_blockdistance_and_time(speed_kmh, brakedelay, safetydistance, mainlength):
     # Speed in m/s
     v = speed_kmh * 1000 / 3600
     
@@ -11,13 +11,17 @@ def calc_blockdistance_and_time(speed_kmh, brakedelay, safetydistance):
     # Time to cover blockdistance (in seconds)
     time_to_cover_blockdistance = blockdistance / v
     
-    return blockdistance, time_to_cover_blockdistance
+    # Blocklength 
+    signalblock = mainlength / blockdistance
 
-# Currently: Speed = 140 km/h, Brakedelay = 0.5 m/s², Safetydistance = 400 m
-blockdistance, time_to_cover_blockdistance = calc_blockdistance_and_time(160, 0.65, 600)
+    return blockdistance, time_to_cover_blockdistance, signalblock 
+
+# Currently: Speed = 140 km/h, Brakedelay = 0.5 m/s², Safetydistance = 400 m, !! Mainlegth = 40 km !!
+blockdistance, time_to_cover_blockdistance, signalblock = calc_blockdistance_and_time(200, 0.65, 600, 110000)
 
 print(f"The blockdistance amounts to: {blockdistance:.2f} meters")
 print(f"The time to cover the blockdistance is: {time_to_cover_blockdistance:.2f} seconds")
+print(f"You have {signalblock:.0f} Blocksignals to place")
 
 # def calc_blocktime (speed_kmh, safetydistance):
     #v = speed_kmh * 1000 / 3600
