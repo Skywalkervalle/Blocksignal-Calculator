2707aa9cf2927a9ac92eba28e8cbf2dcd974abbf
Skywalkervalle <64580908+Skywalkervalle@users.noreply.github.com>
2025-03-31 18:32:08 +0200

    update

--- main_V2.py
+++ main_V2.py
@@ -15,14 +15,14 @@ safetydistance = int (user_safetydistance)
 import time
 import sys
 
-animation = "|/-\\|/-\\"
+animation = "/-\\"
 start_time = time.time()
 while True:
     for i in range(4):
-        time.sleep(0.2)  # Feel free to experiment with the speed here
+        time.sleep(0.1)  # Feel free to experiment with the speed here
         sys.stdout.write("\r" + animation[i % len(animation)])
         sys.stdout.flush()
-    if time.time() - start_time > 10:  # The animation will last for 10 seconds
+    if time.time() - start_time > 5:  # The animation will last for 5 seconds
         break
 sys.stdout.write("\rDone!")
 
